bcb1f747e8fd56063e6420b903e06adb6430bd96
Juliana Farias <juliana.ferreira.farias@gmail.com>
2021-06-21 11:33:13 -0300

    Property Bind ngModel e ngSubmit

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,14 +1,19 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-  selector : 'app-nova-transferencia',
-  templateUrl : 'nova-transferencia.component.html',
-  styleUrls : [ 'nova-transferencia.component.scss' ]
+  selector: 'app-nova-transferencia',
+  templateUrl: 'nova-transferencia.component.html',
+  styleUrls: ['nova-transferencia.component.scss'],
 })
-export class NovaTransferenciaComponent{
+export class NovaTransferenciaComponent {
 
-  transferir(){
+
+  valor: number = 12; /* Iniciar com valor quando for numerico ou adicionar o !*/
+  destino: number = 123; /* Iniciar com valor quando for numerico ou adicionar o !*/
+
+  transferir() {
     console.log('Solicitada nova transferencia');
+    console.log('Valor: ' + this.valor );
+    console.log('Destino: ' + this.destino);
   }
-
 }
